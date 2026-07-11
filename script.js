e979f08d5864c2647f46eedf78a1b9df69f4b9bb
Mr-MohammadHossein-Design <mohammadhossein.tir.1391@gmail.com>
2026-07-11 21:03:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function login() {
 
+    let phone = document.getElementById("phone").value;
+    let password = document.getElementById("password").value;
+
+
+    // اطلاعات آزمایشی ورود
+    let correctPhone = "09123456789";
+    let correctPassword = "123456";
+
+
+    if (phone === correctPhone && password === correctPassword) {
+
+        window.location.href = "download.html";
+
+    } else {
+
+        document.getElementById("message").innerHTML =
+        "شماره تلفن یا رمز عبور اشتباه است";
+
+    }
+
+}
